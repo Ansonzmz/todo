ae59ff23d2657eb289bbb07fe13820554f82c4ae
Anson <zmz211@gmail.com>
2022-10-28 19:28:00 -0700

    first commit with mongo, drafted functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Welcome, Anson</h1>
+            <h4>Your Tasks</h4>
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Hungry ? Get the bread</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
