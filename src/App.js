1d3b2b28f8a08ae56ad033f7ec41551be87d6823
Anson <zmz211@gmail.com>
2022-10-29 22:18:17 -0700

    3rd commit, app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false);
+    const [newTodo, setNewTodo] = useState("");
+
+    useEffect(() => {
+        GetTodos();
+    }, [])
+
+    const GetTodos = () => {
+        fetch(API_BASE + "/todos")
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.error("Error: ", err));
+    }    
+
     return (
         <div className="App">
             <h1>Welcome, Anson</h1>
             <h4>Your Tasks</h4>
             <div className="todos">
-                <div className="todo">
-                    <div className="checkbox"></div>
+                {todos.map(todo => (
+                    <div className="todo">
+                        <div className="checkbox"></div>
 
-                    <div className="text">Get the bread</div>
+                        <div className="text">Get the bread</div>
 
-                    <div className="delete-todo">x</div>
-                </div>
-
-                <div className="todo is-complete">
-                    <div className="checkbox"></div>
-
-                    <div className="text">Get the milk</div>
-
-                    <div className="delete-todo">x</div>
-                </div>
+                        <div className="delete-todo">x</div>
+                    </div>                    
+                ))}
             </div>
         </div>
     );
