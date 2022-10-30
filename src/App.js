34688be60843daf973e9bbf5d42ab832f82ed7ac
Anson <zmz211@gmail.com>
2022-10-30 00:01:32 -0700

    completeTodo

--- src/App.js
+++ src/App.js
@@ -18,16 +18,32 @@ function App() {
             .catch(err => console.error("Error: ", err));
     }    
 
+    const completeTodo = async id => {
+            const data = fetch(API_BASE + "/todo/complete/" + id)
+            .then(res => res.json());
+
+            setTodos(todos => todos.map(todo => {
+                if (todo._id === data._id) {
+                    todo.complete = data.complete;
+                }
+
+                return todo;
+            }));
+    }
+
     return (
         <div className="App">
             <h1>Welcome, Anson</h1>
             <h4>Your Tasks</h4>
+
             <div className="todos">
                 {todos.map(todo => (
-                    <div className="todo">
+                    <div className={
+                        "todo " + (todo.complete ? "is-complete" : "")
+                    } key={todo._id} onClick={() => completeTodo(todo._id)}>
                         <div className="checkbox"></div>
 
-                        <div className="text">Get the bread</div>
+                        <div className="text">{ todo.text }</div>
 
                         <div className="delete-todo">x</div>
                     </div>                    
