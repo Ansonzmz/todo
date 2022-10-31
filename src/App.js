5a9cf01b89860eb16a90edbf5ff76f87b6404de5
Anson <zmz211@gmail.com>
2022-10-31 00:02:40 -0700

    complete, todo, newTodo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     }    
 
     const completeTodo = async id => {
-            const data = fetch(API_BASE + "/todo/complete/" + id)
+            const data = await fetch(API_BASE + "/todo/complete/" + id)
             .then(res => res.json());
 
             setTodos(todos => todos.map(todo => {
@@ -29,6 +29,28 @@ function App() {
 
                 return todo;
             }));
+    }
+
+    const deleteTodo = async id => {
+        const data = await fetch(API_BASE + "/todo/delete/" + id, { 
+            method: "DELETE"
+        }).then(res => res.json());
+
+        setTodos(todos => todos.filter(todo => todo._id !== data._id))
+    }
+
+    const addTodo = async () => {
+        const data = await fetch(API_BASE + "/todo/new", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                text: newTodo
+            })
+        }).then(res => res.json());
+
+        setTodos([...todos, data]);
     }
 
     return (
@@ -45,10 +67,27 @@ function App() {
 
                         <div className="text">{ todo.text }</div>
 
-                        <div className="delete-todo">x</div>
+                        <div className="delete-todo" onClick={() => deleteTodo(todo._id)} >x</div>
                     </div>                    
                 ))}
             </div>
+
+            <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+            {popupActive ? (
+                <div className="popup">
+                    <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+                    <div className="content">
+                        <h3>Add Task</h3>
+                        <input 
+                            type="text" 
+                            className="add-todo-input" 
+                            onChange={e => setNewTodo(e.target.value)}
+                            value={newTodo} />
+                            <div className="button" onClick={addTodo} >Create Task</div>
+                    </div>
+                </div>
+            ) : ''}
         </div>
     );
 }
